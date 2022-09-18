f6b53ebfba7c5f68235790e79d12714a87a7d6b0
Admin <budda4613@gmail.com>
2022-09-19 00:21:27 +0500

    so many qestions

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        char a= 97;
+        char b= 98;
+        int formyla = (9 * (a * a) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+                System.out.print(formyla);
+
+
+
+
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c=99;
+        char h=104;
+        char a=97;
+        char r=114;
+        System.out.print(c+h+a+r);
+
     }
 }
